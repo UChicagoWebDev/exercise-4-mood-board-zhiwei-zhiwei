38eb2e95e95d748d7756320d6debaeb823fa19f8
Zhiwei Cao <zhiweic@uchicago.edu>
2024-01-31 14:04:12 -0600

    submit

--- mood.js
+++ mood.js
@@ -1,14 +1,50 @@
 const bing_api_endpoint = "https://api.bing.microsoft.com/v7.0/images/search";
-const bing_api_key = BING_API_KEY
+const bing_api_key = BING_API_KEY;
+
+function displaySuggestions(queryExpansions) {
+  const suggestions = document.querySelector(".suggestions ul");
+  suggestions.innerHTML = "";
+  queryExpansions.forEach(element => {
+      const li = document.createElement("li");
+      li.textContent = element.displayText;
+      li.onclick = () => {
+          document.querySelector(".search input").value = element.displayText;
+          runSearch();
+      };
+      suggestions.appendChild(li);
+  });
+}
+
+function displayImgOnBoard(image) {
+  const board = document.getElementById("board");
+  const div = document.createElement("div");
+  div.className = "savedImage";
+  const img = document.createElement("img");
+  img.src = image;
+  div.appendChild(img);
+  board.appendChild(div);
+}
 
 function runSearch() {
 
   // TODO: Clear the results pane before you run a new search
+  const container = document.getElementById("resultsImageContainer");
+  while (container.firstChild) {
+      container.removeChild(container.firstChild);
+  }
+
+  // const preveContainer = document.getElementById("resultsImageContainer");
+  // suggestionsContainer.innerHTML = "";
+  // document.getElementById('resultsImageContainer').innerHTML = '';
+
 
   openResultsPane();
 
   // TODO: Build your query by combining the bing_api_endpoint and a query attribute
   //  named 'q' that takes the value from the search bar input field.
+  const user_input = document.querySelector(".search input").value.trim();
+  const query_url = `${bing_api_endpoint}?q=${encodeURIComponent(user_input)}`;
+
 
   let request = new XMLHttpRequest();
 
@@ -25,8 +61,47 @@ function runSearch() {
   //   - HINT: You'll need to ad even listeners to them after you add them to the DOM
   //
   // request.setRequestHeader("Ocp-Apim-Subscription-Key", bing_api_key);
+  
+  request.open("GET", query_url);
+  request.responseType = 'json';
+  request.setRequestHeader("Ocp-Apim-Subscription-Key", bing_api_key);
+  console.log(request);
+
+  request.onload = function(event) {
+    if (request.status >= 200 && request.status < 300) {
+      // const img_source = request.response.value;
+      
+      // const img_source = event.target.response;
+      // console.log(img_source);
+      // const resImg = document.getElementById("resultsImageContainer");
+
+      const response = event.target.response;
+      console.log(response);
+      const images = response.value;
+      const queryExpansions = response.queryExpansions;
+
+      displaySuggestions(queryExpansions);
+
+      images.forEach(element => {
+        console.log(element);
+        const div = document.createElement("div");
+        div.className = "res_image";
+        const img = document.createElement("img");
+        img.src = element.thumbnailUrl;
+        img.alt = element.name;
+
+        img.onclick = () => displayImgOnBoard(element.contentUrl);
+
+        div.appendChild(img);
+        document.getElementById("resultsImageContainer").appendChild(div);
+      });
+    }else{
+      console.error("Request failed with status: ", request.status);
+    }
+  }
 
   // TODO: Send the request
+  request.send();
 
   return false;  // Keep this; it keeps the browser from sending the event
                   // further up the DOM chain. Here, we don't want to trigger
